737cb05b80c8d6f17e018ea424b4788a7fc72523
bmirona <b.mirona@yahoo.com>
2016-04-03 16:52:34 +0200

    creation of AdjMatrixGraph &TestGraphImpl classes

---
+++ src/graphImpl/TestGraphImpl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graph;
+
+/**
+ *
+ * @author Miro boss
+ */
+public class TestGraphImpl {
+     public static void main(String[] args) {
+
+    
+
+    // Create an 3-vertex graph.
+    System.out.println("Creating a graph with 3 vertices");
+    AdjMatrixGraph graph = new AdjMatrixGraph(3);
+    graph.addEdge(0, 2);
+    graph.addEdge(1, 0);
+    
+    System.out.println("\nThe graph has\n" + graph);
+   
+}
+}
