657600ad18ff990628b8691d78f22feb87dbf3dc
Tombean <tom.somervilleroberts@gmail.com>
2016-04-06 15:53:48 +0200

    f

--- src/graphImpl/TestGraphImpl.java
+++ src/graphImpl/TestGraphImpl.java
@@ -16,17 +16,18 @@ public class TestGraphImpl {
     
 
     // Create an 3-vertex graph.
-    System.out.println("Creating a graph with 5 vertices maximum");
-    AdjMatric graph = new AdjMatric(5, false);
-    double v1Coord[] = {0.3, 0.0};
-    double v2Coord[] = {1.0, 1.2};
-    double v3Coord[] = {2.0, -2.0};
-    Vertex v1 = new Vertex(v1Coord);
-    System.out.println("Adding Vertex v1 with the given coordinates :  ( " + v1.getCoordinates()[0] + " , "+v1.getCoordinates()[1] + " ) ");
-    Vertex v2 = new Vertex(v2Coord);
-    System.out.println("Adding Vertex v2 with the given coordinates :  ( " + v2.getCoordinates()[0] + " , "+v2.getCoordinates()[1] + " ) ");
-    Vertex v3 = new Vertex(v3Coord);
-    System.out.println("Adding Vertex v3 with the given coordinates :  ( " + v3.getCoordinates()[0] + " , "+v3.getCoordinates()[1] + " ) ");
+    int numberMaximumOfVertices = 5;
+    System.out.println("Creating a graph with "+ numberMaximumOfVertices+" vertices maximum");
+    AdjMatric graph = new AdjMatric(numberMaximumOfVertices, false);
+    String nomV1 = "A";
+    String nomV2 = "B";
+    String nomV3 = "C";
+    Vertex v1 = new Vertex(nomV1);
+    System.out.println("Adding Vertex v1 with the given coordinates :  ( " + v1.getCoordinates() + " ) ");
+    Vertex v2 = new Vertex(nomV2);
+    System.out.println("Adding Vertex v2 with the given coordinates :  ( " + v2.getCoordinates() + " ) ");
+    Vertex v3 = new Vertex(nomV3);
+    System.out.println("Adding Vertex v3 with the given coordinates :  ( " + v3.getCoordinates() + " ) ");
     UndirectedEdge e1 = new UndirectedEdge(v1, v2);
     UndirectedEdge e2 = new UndirectedEdge(v2, v3);
     System.out.println("Adding Edge e1 ( v1, v2 ) ..... e1 was successfully created  :  " + graph.addEdgeToGraph(v1, v2, (Edge)e1));
@@ -35,10 +36,10 @@ public class TestGraphImpl {
     System.out.println("Existence of an Edge bewteen ( v1, v3 ) :  " + graph.hasEdge(v1, v3) );
     System.out.println("Checking if e1 and e2 are adjacent ...... Adjacency :  " + graph.isAdjacent( e1, e2 ) );
     Vertex[] neighbours = graph.getNeighbours(v2);
-    System.out.println("Neighbours of v2 are : ");
-    for ( int i = 0; i < neighbours.length ; i++){ System.out.print(neighbours[i].getClass().getSimpleName() +" (" + neighbours[i].getCoordinates()[0] + ", " + neighbours[i].getCoordinates()[1] + ") // "); }
+    System.out.println("Neighbours of "+v2.getCoordinates() +" are : ");
+    for ( int i = 0; i < neighbours.length ; i++){ System.out.print(neighbours[i].getClass().getSimpleName() +" ( " + neighbours[i].getCoordinates() + " ) // "); }
     
-    System.out.println("\nThe graph has\n" + graph);
+    System.out.println("\nThe graph is called : " + graph.toString());
     
    
 }
